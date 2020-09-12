cd981e02d92f90305f7f13b0507d7edcab8bc9fa
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-12 12:42:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <? php
 
 require_once 'Mobile-Detect-master/Mobile-Detect-master/Mobile_Detect.php';
-$detect = new Mobile_Detect;
-if($detect->isAndroidOS()) {
-    header('Location: http://android.com/');
-   
-}else if($detect->isiPhone()){
-    header('Location: http:/iphone.com/');
-    
+if($detect->isMobile() || $detect->isTablet()) {
+    echo "<link rel="stylesheet" href="mobile.css type=" text="" css'="">";
+} else {
+    echo "<link rel="stylesheet" href="style.css type=" text="" css'="">";
 }
-
 
 
 
