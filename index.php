a0fb9a98ec6930e615b8830ce483b1efd333d606
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-12 12:32:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <? php
 
-require_once 'Mobile_Detect.php';
+require_once 'Mobile-Detect-master/Mobile-Detect-master/Mobile_Detect.php';
 $detect = new Mobile_Detect;
 if($detect->isAndroidOS()) {
     header('Location: http://android.com/');
