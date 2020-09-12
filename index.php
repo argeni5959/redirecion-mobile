bd3a263befc53160bbaf793de843544cf826259c
argeni5959 <67845811+argeni5959@users.noreply.github.com>
2020-09-12 12:52:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
  <?php
 
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
 
-
+if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+ {
      echo "https://s3.amazonaws.com/woocms.woorank.com/2020/05/poor-mobile-optimization.png";
-  
+  }
  ?>
 
 
